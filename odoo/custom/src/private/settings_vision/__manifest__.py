f42cb0c38dd2d6ddba894fa6d7806b95a04a87a9
bassbassbuff <bassbassbuff@gmail.com>
2023-08-14 11:01:05 +0300

    Settings app

---
+++ odoo/custom/src/private/settings_vision/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Vision Settings",
+    "version": "15.0.1.1.1",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "summary": "Install custom settings for vision Energy",
+    "depends": [
+        "stock",
+        "l10n_ke",
+        "purchase",
+        "stock_account",
+        "purchase_stock",
+        "account",
+        "stock_landed_costs",
+        "product_margin",
+        "stock_landed_costs_purchase_auto",
+    ],
+    "installable": True,
+    "auto_install": True,
+    "application": False,
+    "post_init_hook": "post_init_hook",
+}
